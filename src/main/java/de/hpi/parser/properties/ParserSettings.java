74b0a311cef332c3484e8f4e05989f50b6592c7b
deeps96 <leonardo.huebscher.business@gmail.com>
2018-05-14 16:04:33 +0200

    Add retry dependency and jsonPath dependency. #11

--- src/main/java/de/hpi/parser/properties/ParserSettings.java
+++ src/main/java/de/hpi/parser/properties/ParserSettings.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableConfigurationProperties
+@EnableRetry
 @ConfigurationProperties("parser-settings")
 @Getter
 @Setter
