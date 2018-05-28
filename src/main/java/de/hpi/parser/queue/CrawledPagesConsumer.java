f1d9fb555953faa0508e37eba98b1a296d8d7c9a
deeps96 <leonardo.huebscher.business@gmail.com>
2018-05-28 09:34:07 +0200

    Add delay between rabbit message fetch if thread limit is exceeded. #14

--- src/main/java/de/hpi/parser/queue/CrawledPagesConsumer.java
+++ src/main/java/de/hpi/parser/queue/CrawledPagesConsumer.java
@@ -1,6 +1,7 @@
 package de.hpi.parser.queue;
 
 import de.hpi.parser.dto.CrawledPage;
+import de.hpi.parser.properties.ParserConfig;
 import de.hpi.parser.service.IParserService;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.core.task.TaskRejectedException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,9 +21,16 @@ public class CrawledPagesConsumer {
 
     private final IParserService parserService;
 
+    private final ParserConfig config;
+
     @RabbitListener(queues = "#{@crawledPages}")
-    public void onMessage(CrawledPage crawledPage) {
+    public void onMessage(CrawledPage crawledPage) throws InterruptedException {
         log.info("Received crawled page from shop " + crawledPage.getShopId() + " with url " + crawledPage.getUrl());
-        getParserService().extractData(crawledPage);
+        try {
+            getParserService().extractData(crawledPage);
+        } catch (TaskRejectedException e) {
+            log.info("Thread pool has no free capacity.", e);
+            Thread.sleep(getConfig().getWaitIfThreadCapacityReachedInMilliseconds());
+        }
     }
 }
