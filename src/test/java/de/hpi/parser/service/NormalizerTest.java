fa6636f1dd22edc3ac6fdc0e3bf8f6f963117981
deeps96 <leonardo.huebscher.business@gmail.com>
2018-06-04 10:38:10 +0200

    Fix normalizer test.

--- src/test/java/de/hpi/parser/service/NormalizerTest.java
+++ src/test/java/de/hpi/parser/service/NormalizerTest.java
@@ -12,21 +12,11 @@ public class NormalizerTest {
 
     @Test
     public void normalizePrice() {
-        Map<OfferAttribute, String> data = new EnumMap<>(OfferAttribute.class);
-        Arrays.stream(OfferAttribute.values()).forEach(offerAttribute -> data.put(offerAttribute, ""));
-        data.put(OfferAttribute.PRICE, "14.99");
-        Normalizer.normalizeData(data);
-
-        assertEquals("1499", data.get(OfferAttribute.PRICE));
+        assertEquals("1499", Normalizer.normalizeData("14.99", OfferAttribute.PRICE));
     }
 
     @Test
     public void normalizeEAN() {
-        Map<OfferAttribute, String> data = new EnumMap<>(OfferAttribute.class);
-        Arrays.stream(OfferAttribute.values()).forEach(offerAttribute -> data.put(offerAttribute, ""));
-        data.put(OfferAttribute.EAN, "00123/07544");
-        Normalizer.normalizeData(data);
-
-        assertEquals("123, 7544", data.get(OfferAttribute.EAN));
+        assertEquals("123, 7544", Normalizer.normalizeData("00123/07544", OfferAttribute.EAN));
     }
 }
